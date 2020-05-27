a60f1f49a1c35c511f9e496275ade656a8dd7168
laopeng <521134148899>
2020-05-27 21:57:44 +0800

    0527

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+   <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对谢可寅的看法</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+        <Add :addComment="addComment"/>
+        <List :comments="comments" :deleteComment="deleteComment"/>
+    </div>
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+
+import Add from './components/Add'
+import List from '@/components/List'
+
+export default {
+    components:{
+        Add:Add,
+        List:List
+    },
+    data(){
+        return {
+            comments:[
+                {id:1,username:'小李',content:'谢可寅绝美'},
+                {id:2,username:'老刘',content:'这小姑娘实力派'},
+                {id:3,username:'小蔡',content:'喜欢可寅呐'}
+            ]
+        }
+    },
+    methods:{
+        addComment(obj){
+            this.comments.unshift(obj)
+        },
+        deleteComment(index){
+            this.comments.splice(index,1)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry:'./src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+           
+            {
+                test: /\.css$/,
+                use: [ 'vue-style-loader', 'css-loader' ]
+            },
+           
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192*100, 
+                            name:'[hash:8].[ext]' 
+                        }
+                    }
+                ]
+            },
+           
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./src/public/index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new VueLoaderPlugin(),
+        new CopyPlugin([ 
+            {
+                from:path.resolve(__dirname,'src/public'),
+                to:path.resolve(__dirname,'dist'),
+                ignore:['index.html']
+            }
+        ]),
+
+    ],
+
+    mode:'development',
+
+    devServer: {
+        port:8080,
+        open:true,
+        quiet:true,
+    },
+
+    devtool:'cheap-module-eval-source-map',
+
+    resolve:{
+        extensions: [".js", ".json",".vue"],
+        alias:{
+            '@': path.resolve(__dirname, 'src')
+        }
+    }
+
+};
